b971272b83ba091b8047bb564f9c27ad8426d218
Anton <antonzo93@mail.ru>
2023-03-11 22:08:14 +0300

    add Example005_HelloNastya

---
+++ Example005_HelloNastya/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Ведите имя пользователя");
+string username = Console.ReadLine();
+
+if(username == "Настя")
+{
+    Console.WriteLine("Ура! это же НАСТЯ!");
+}
+
+else
+{
+    Console.WriteLine("Здраствуйте,");
+    Console.WriteLine(username);
+}
