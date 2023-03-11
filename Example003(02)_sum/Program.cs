ad63cd9480c63524322d97c26d490f665a29d4ed
Anton <antonzo93@mail.ru>
2023-03-11 21:33:35 +0300

    add Example003(02)_sum

---
+++ Example003(02)_sum/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Программа для вычесления рандомных чисел А + В или А - В");
+
+int numberA = new Random().Next(1, 10); // Диапозон от 1 до 10
+int numberB = new Random().Next(1, 10); // Диапозон от 1 до 10
+
+Console.Write("Первое рандомное число - "); Console.WriteLine(numberA); 
+Console.Write("Второе рандомное число - "); Console.WriteLine(numberB);
+
+int result = numberA + numberB;
+
+Console.Write("ИТОГО получаем число : "); Console.WriteLine(result);
